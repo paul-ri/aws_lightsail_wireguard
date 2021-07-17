dbdd7d16ea500daf5856f8e2dd0545fb9a31b58b
Paul Riou <paul.riou@protonmail.com>
2021-07-17 23:37:07 +0100

    tf:Add aws_profile_name variable

--- main.tf
+++ main.tf
@@ -4,6 +4,7 @@ provider "null" {}
 provider "tls" {}
 
 provider "aws" {
+  profile = var.aws_profile_name
   region = var.aws_region
 }
 
--- vars.tf
+++ vars.tf
@@ -3,4 +3,10 @@ variable "SERVER_PUBLICKEY" {}
 variable "CLIENT_PRIVATEKEY" {}
 variable "CLIENT_PUBLICKEY" {}
 
+variable "aws_profile_name" {
+  description = "AWS profile to use"
+  type        = string
+  default     = "default"
+}
+
 variable "aws_region" {}
